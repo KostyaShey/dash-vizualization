5666de41df0b2b7c41e9bdc9c3cbb10e8236e23a
KostyaShey <sheynkler87@gmail.com>
2019-09-20 11:51:04 +0200

    setting up circleCI

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,19 @@
-print(2+2)
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#function for the first CI step
+def addtwonumbers(a,b):
+    return a+b
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[html.H1(children='Dash Tutorials'),\
+    dcc.Graph(id='example',\
+        figure={'data': [\
+            {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 9, 9, 9], 'type': 'line', 'name': 'Boats'},\
+            {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},],\
+                'layout': {'title': 'Basic Dash Example'}})])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ test_oftheboard.py
@@ -0,0 +1,6 @@
+import dashboard
+
+class TestDashboard:
+
+    def test_addition(self):
+        assert 4 == dashboard.addtwonumbers(2, 2)
