427d5a713c1a2c93e9a66595710226786baefe60
KostyaShey <sheynkler87@gmail.com>
2019-09-23 14:16:32 +0200

    first graph online! using data from USvideos

--- dashboard.py
+++ dashboard.py
@@ -1,43 +1,62 @@
-import pandas_datareader.data as web
 import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import pandas as pd
 
 app = dash.Dash()
 
+def generate_dropdown_options():
+    
+    dropdown_options = []
+    df_options = pd.read_csv('USvideos.csv')
+    df_options = df_options[["title", "video_id"]]
+    df_options.drop_duplicates(inplace = True)
+    df_options.columns = ['label','value']
+    for index, row in df_options.iterrows():
+        dropdown_options.append(row.to_dict())
+    return dropdown_options
+
+
 app.layout = html.Div(children=[
     html.Div(children='''
-        Symbol to graph:
+        Video to graph:
     '''),
-    dcc.Input(id='input', value='', type='text'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=generate_dropdown_options(),
+        value="",
+        placeholder="Select a video"
+    ),
     html.Div(id='output-graph'),
 ])
 
 @app.callback(
     Output(component_id='output-graph', component_property='children'),
-    [Input(component_id='input', component_property='value')]
+    [Input(component_id='my-dropdown', component_property='value')]
 )
 def update_value(input_data):
-    start = datetime.datetime(2015, 1, 1)
-    end = datetime.datetime.now()
-    df = web.DataReader(input_data, 'morningstar', start, end)
+
+    df = pd.read_csv('USvideos.csv')
     df.reset_index(inplace=True)
-    df.set_index("Date", inplace=True)
-    df = df.drop("Symbol", axis=1)
+    df = df[df["video_id"] == input_data]
+    df.reset_index(inplace=True)
+    if input_data != "":
+        title_for_graph = "Views per day for " + str(df["title"].unique()[0])
 
     return dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},
+                {'x': df["trending_date"], 'y': df["views"], 'type': 'line', 'name': input_data},
             ],
             'layout': {
-                'title': input_data
+                'title': title_for_graph
             }
         }
     )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- test_oftheboard.py
+++ test_oftheboard.py
@@ -2,5 +2,4 @@ import dashboard
 
 class TestDashboard:
 
-    def test_addition(self):
-        assert 4 == dashboard.addtwonumbers(2, 2)
+    pass
