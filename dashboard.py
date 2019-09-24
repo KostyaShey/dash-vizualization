916e76824b5ef482a58927b460615a57d85267c7
KostyaShey <sheynkler87@gmail.com>
2019-09-24 13:42:02 +0200

    editet the dashboard to display dinamic oil price

--- dashboard.py
+++ dashboard.py
@@ -1,62 +1,57 @@
-import datetime
 import dash
+from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import pandas as pd
+import plotly
+import random
+import plotly.graph_objs as go
+from collections import deque
+import requests
+from datetime import datetime
 
 app = dash.Dash()
 
-def generate_dropdown_options():
-    
-    dropdown_options = []
-    df_options = pd.read_csv('USvideos.csv')
-    df_options = df_options[["title", "video_id"]]
-    df_options.drop_duplicates(inplace = True)
-    df_options.columns = ['label','value']
-    for index, row in df_options.iterrows():
-        dropdown_options.append(row.to_dict())
-    return dropdown_options
+headers = {'Authorization': 'Token e9e301d24df5800184e89343cf47381b', 'Content-Type': 'application/json'}
+response = requests.get("https://api.oilpriceapi.com/v1/prices/latest", headers=headers).json()
 
 
-app.layout = html.Div(children=[
-    html.Div(children='''
-        Video to graph:
-    '''),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=generate_dropdown_options(),
-        value="",
-        placeholder="Select a video"
-    ),
-    html.Div(id='output-graph'),
-])
+now = datetime.now()
+X = deque(maxlen=20)
+X.append(now.strftime("%H:%M:%S"))
+Y = deque(maxlen=20)
+Y.append(response["data"]["price"])
 
-@app.callback(
-    Output(component_id='output-graph', component_property='children'),
-    [Input(component_id='my-dropdown', component_property='value')]
+app.layout = html.Div(
+    [
+        html.H1('Oil Price live update'),
+        dcc.Graph(id='live-graph', animate=True),
+        dcc.Interval(
+            id='graph-update',
+            interval=1*30000,
+            n_intervals=0
+        ),
+    ]
 )
-def update_value(input_data):
 
-    df = pd.read_csv('USvideos.csv')
-    df.reset_index(inplace=True)
-    df = df[df["video_id"] == input_data]
-    df.reset_index(inplace=True)
-    if input_data != "":
-        title_for_graph = "Views per day for " + str(df["title"].unique()[0])
+@app.callback(Output('live-graph', 'figure'), [Input('graph-update', 'n_intervals')])
 
-    return dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': df["trending_date"], 'y': df["views"], 'type': 'line', 'name': input_data},
-            ],
-            'layout': {
-                'title': title_for_graph
-            }
-        }
-    )
+def update_graph_scatter(n):
+    
+    response = requests.get("https://api.oilpriceapi.com/v1/prices/latest", headers=headers).json()
+    now = datetime.now()
 
+    X.append(now.strftime("%H:%M:%S"))
+    Y.append(response["data"]["price"])
+
+    data = plotly.graph_objs.Scatter(
+            x=list(X),
+            y=list(Y),
+            name='Scatter',
+            mode= 'lines+markers'
+            )
+
+    return {'data': [data], 'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
+                                                yaxis=dict(range=[min(Y),max(Y)]),)}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
